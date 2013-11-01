893c0b65afe22db8e4c409dffae9936c0530397b
huangzhaoqing <huangzhaoqing@goodow.com>
2013-11-01 09:51:53 +0800

    更改我的课程展示方式和model使用错误
    Change-Id: I33cf7e275ff7b19f03a68f27076ba9166141a1d8

--- Project/GDDrive/UI/ipad/GDDClassViewController_iPad.m
+++ Project/GDDrive/UI/ipad/GDDClassViewController_iPad.m
@@ -75,11 +75,15 @@ static NSString * FILES_KEY = @"files";
 #pragma mark -tableView dataSource
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-  return 1;
+  return 2;
 }
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-  return [self.folderList length] + [self.filesList length];
+  return section == 0 ? [self.folderList length] : [self.filesList length];
+}
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+  
+  return section == 0 ? @"文件夹" : @"文件";
 }
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
@@ -88,11 +92,20 @@ static NSString * FILES_KEY = @"files";
   if (cell == nil) {
     cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
   }
-  if ([self.folderList length]>0) {
-    GDRCollaborativeMap *map = [self.folderList get:indexPath.row];
-    cell.textLabel.text = [map get:@"label"];
+  if (indexPath.section == 0) {
+    if ([self.folderList length]>0) {
+      GDRCollaborativeMap *map = [self.folderList get:indexPath.row];
+      cell.textLabel.text = [map get:@"label"];
+    }
+    return cell;
+  }else{
+    if ([self.filesList length]>0) {
+      GDRCollaborativeMap *map = [self.filesList get:indexPath.row];
+      cell.textLabel.text = [map get:@"label"];
+    }
+    return cell;
   }
-  return cell;
+  
 }
 #pragma mark - tableView delegate
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
